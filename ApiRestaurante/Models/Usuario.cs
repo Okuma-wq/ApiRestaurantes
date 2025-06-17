b5b28d0d7a41349555f9c0af7359407de1661a57
Pedro Laurentino Okuma <pedrookuma3@gmail.com>
2025-06-16 22:07:26 -0300

    ajuste usuário

--- ApiRestaurante/Models/Usuario.cs
+++ ApiRestaurante/Models/Usuario.cs
@@ -9,15 +9,22 @@ namespace API.Models
         [BsonRepresentation(BsonType.String)]
         public string? Id { get; set; }
 
+        [BsonElement("nome")]
         public string Nome { get; set; } = string.Empty;
 
+        [BsonElement("email")]
         public string Email { get; set; } = string.Empty;
 
+        [BsonElement("senha")]
         public string Senha { get; set; } = string.Empty;
+
+        [BsonElement("foto")]
+        public string Foto { get; set; } = string.Empty;
 
         [BsonElement("data_cadastro")]
         public DateTime DataCadastro { get; set; }
 
+        [BsonElement("favoritos")]
         public List<string?> Favoritos { get; set; } = new();
     }
 }
