a713afb6063986ec23330caf244be5bc4dd23ee2
Pedro Laurentino Okuma <pedro.okuma@sesisenaisp.org.br>
2026-05-22 20:04:50 -0300

    ajuste

--- ApiRestaurante/DTOs/ReviewCriarDto.cs
+++ ApiRestaurante/DTOs/ReviewCriarDto.cs
@@ -8,7 +8,6 @@ public class ReviewCriarDto
 {
     public string? IdRestaurante { get; set; }
     public string? NomeRestaurante { get; set; }
-    public string? IdUsuario { get; set; }
     public int Nota { get; set; }
     public string Comentario { get; set; } = string.Empty;
     public DateTime Data { get; set; } = DateTime.UtcNow;
